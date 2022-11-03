3269aebff28061389a78bb173a0b685256bda567
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2022-11-03 21:37:27 +0900

    JAVA

---
+++ 프로그래머스/Level_1/최소직사각형.java
@@ -0,0 +1,18 @@
+import java.util.Arrays; // sort
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int[] w = new int[sizes.length];
+        int[] h = new int[sizes.length];
+        for(int i=0; i<sizes.length; i++){
+            h[i] = Math.min(sizes[i][0],sizes[i][1]);
+            w[i] = Math.max(sizes[i][0],sizes[i][1]);
+        }
+        Arrays.sort(h);
+        Arrays.sort(w);
+        answer = h[sizes.length-1] * w[sizes.length-1] ;
+        
+        return answer;
+    }
+}
