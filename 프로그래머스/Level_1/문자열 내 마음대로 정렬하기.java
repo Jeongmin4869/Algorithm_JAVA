0623b291f9c4c0eec0896cd7a099ecd0be5e79bd
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-03-14 21:59:47 +0900

    Java

---
+++ 프로그래머스/Level_1/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = new String[strings.length];
+        for(int i=0; i<strings.length; i++){
+            answer[i] = strings[i].substring(n, n+1) + strings[i];
+        }
+        Arrays.sort(answer);
+        for(int i=0; i<strings.length; i++){
+            answer[i] = answer[i].substring(1, answer[i].length());
+        }
+
+        
+        return answer;
+    }
+}
