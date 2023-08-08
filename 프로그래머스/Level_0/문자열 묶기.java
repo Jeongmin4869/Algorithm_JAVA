e5bbb945100b05b029088caebe226a74cdad7bca
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-08-08 22:28:18 +0900

    Java

---
+++ 프로그래머스/Level_0/문자열 묶기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] counts = new int[31];
+        for(int i=0; i<strArr.length; i++){
+            counts[strArr[i].length()] += 1;
+        }
+        
+        for(int i=1; i<=30; i++){
+            answer = Math.max(answer, counts[i]);
+        }
+        
+        return answer;
+    }
+}
