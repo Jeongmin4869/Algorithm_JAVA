effa13ef0b3e45f90a60d5a3c56d522ab0119e06
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-08-15 20:17:18 +0900

    Java

--- 프로그래머스/Level_0/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/Level_0/조건에 맞게 수열 변환하기 2.java
@@ -3,17 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(int[] arr) {
         int answer = 0;
+        int[] aftarr = arr.clone();
         int[] prearr = arr.clone();
         while(true){
             answer += 1;
-            for(int i=0; i<arr.length; i++){
-                if(arr[i] % 2 == 0 && arr[i] >= 50) arr[i]/=2;
-                else if (arr[i] % 2 !=  0 && arr[i] < 50) arr[i] = arr[i] * 2 + 1;
+            for(int i=0; i<aftarr.length; i++){
+                if(aftarr[i] % 2 == 0 && aftarr[i] >= 50) aftarr[i]/=2;
+                else if (aftarr[i] % 2 !=  0 && aftarr[i] < 50) aftarr[i] = aftarr[i] * 2 + 1;
             }
-            if(Arrays.equals(arr, prearr)){
+            if(Arrays.equals(aftarr, prearr)){
                 break;
             }
-            prearr = arr.clone();
+            prearr = aftarr.clone();
         }
         return answer-1;
     }
