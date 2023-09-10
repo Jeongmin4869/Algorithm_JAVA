c4863519a805f72b607b79418cebef2bdf6ab2d7
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-09-10 17:32:59 +0900

    Java
    System.arraycopy();

--- 프로그래머스/Level_0/정사각형으로 만들기.java
+++ 프로그래머스/Level_0/정사각형으로 만들기.java
@@ -1,8 +1,9 @@
+
 class Solution {
     public int[][] solution(int[][] arr) {
         int max = Math.max(arr.length,arr[0].length);
         int[][] answer = new int[max][max];
-        
+        /*
         for(int i=0; i<max; i++){
             for(int j=0; j<max; j++){
                 if(i >= arr.length){
@@ -16,6 +17,10 @@ class Solution {
                     answer[i][j] = arr[i][j];
                 }
             }
+        }
+        */
+        for(int i=0; i<arr.length; i++){
+            System.arraycopy(arr[i], 0, answer[i], 0, arr[i].length);
         }
         
         return answer;
