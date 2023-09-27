4a9c033bdac3cb4145fe7a1a5bcb077ba4e58789
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-09-27 19:12:39 +0900

    Java

---
+++ 프로그래머스/Level_0/특이한 정렬.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = new int[numlist.length];
+        int[][] arr = new int[numlist.length][2];
+        
+        for(int i=0; i<numlist.length; i++){
+            arr[i][0] = Math.abs(numlist[i]-n);
+            arr[i][1] = numlist[i];
+        }
+        
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[0] == o2[0]) 
+                return Integer.compare(o2[1], o1[1]);
+            else 
+                return Integer.compare(o1[0], o2[0]);
+        });
+        
+        for(int i=0; i<arr.length; i++){
+            answer[i] = arr[i][1];
+        }
+        
+        return answer;
+    }
+}
+
+// https://gre-eny.tistory.com/2
