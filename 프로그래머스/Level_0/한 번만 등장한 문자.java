9fdfa32755a03b9d268cd5d00b2c649a359b30ca
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-09-30 13:51:36 +0900

    Java

---
+++ 프로그래머스/Level_0/한 번만 등장한 문자.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        Map<Character,Integer> m = new HashMap<>();
+        for(int i =0 ; i< s.length(); i++){
+            m.put(s.charAt(i),m.getOrDefault(s.charAt(i),0)+1);  
+}
+
+        for (Character key : m.keySet()) {
+    if(m.get(key)==1) answer += key;
+}
+        
+        return answer;
+    }
+}
