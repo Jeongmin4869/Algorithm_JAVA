57ed58b4dedf8dd9178aa4ca8dbea0667c393c6f
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-10-01 19:42:55 +0900

    Java
    TreeMap

--- 프로그래머스/Level_0/한 번만 등장한 문자.java
+++ 프로그래머스/Level_0/한 번만 등장한 문자.java
@@ -1,15 +1,17 @@
-import java.util.*;
+import java.util.Map;
+import java.util.TreeMap; // Key를 기준으로 자동정렬
+
 class Solution {
     public String solution(String s) {
         String answer = "";
-        Map<Character,Integer> m = new HashMap<>();
-        for(int i =0 ; i< s.length(); i++){
+        Map<Character,Integer> m = new TreeMap<>();
+        for(int i=0 ; i< s.length(); i++){
             m.put(s.charAt(i),m.getOrDefault(s.charAt(i),0)+1);  
-}
+        }       
 
         for (Character key : m.keySet()) {
-    if(m.get(key)==1) answer += key;
-}
+            if(m.get(key)==1) answer += key;
+        }
         
         return answer;
     }
