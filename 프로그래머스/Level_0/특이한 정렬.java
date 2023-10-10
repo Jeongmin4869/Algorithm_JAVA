9d4e3d75c51d53f6758efa65c00d1419d3d5c142
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-10-10 21:00:32 +0900

    Java
    Stream

--- 프로그래머스/Level_0/특이한 정렬.java
+++ 프로그래머스/Level_0/특이한 정렬.java
@@ -1,3 +1,4 @@
+/*
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] numlist, int n) {
@@ -23,5 +24,18 @@ class Solution {
         return answer;
     }
 }
+*/
 
-// https://gre-eny.tistory.com/2
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        
+        return Arrays.stream(numlist)
+            .boxed()
+            .sorted((a,b) -> Math.abs(a-n) == Math.abs(b-n) ? b.compareTo(a) : Integer.compare(Math.abs(a-n), Math.abs(b-n)))
+            .mapToInt(Integer :: intValue)
+            .toArray();
+        
+    }
+}
+
