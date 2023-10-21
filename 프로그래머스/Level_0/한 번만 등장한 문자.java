18f1b21edf15a8c7a477af0112924c75e2d0672d
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-10-22 02:14:57 +0900

    Java
    Stream

--- 프로그래머스/Level_0/한 번만 등장한 문자.java
+++ 프로그래머스/Level_0/한 번만 등장한 문자.java
@@ -1,8 +1,11 @@
 import java.util.Map;
 import java.util.TreeMap; // Key를 기준으로 자동정렬
+import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Solution {
     public String solution(String s) {
+        /*
         String answer = "";
         Map<Character,Integer> m = new TreeMap<>();
         for(int i=0 ; i< s.length(); i++){
@@ -14,5 +17,16 @@ class Solution {
         }
         
         return answer;
+        */
+        
+        return Arrays.stream(s.split(""))
+            .collect(Collectors.groupingBy(s1 -> s1))
+            .entrySet()
+            .stream()
+            .filter(entry -> entry.getValue().size() <= 1)
+            .map(Map.Entry::getKey)
+            .sorted()
+            .collect(Collectors.joining());
+        
     }
 }
