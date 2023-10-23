b7969c54256ee3d3a714c3f329d4a9e01c7dce1f
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-10-23 21:54:33 +0900

    Java

---
+++ 프로그래머스/Level_0/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        
+        int size = 1;
+        while(size < arr.length){
+            size *= 2;
+            if(size == arr.length) break;
+        }
+        
+        int[] answer = new int[size];
+        
+        for(int i=0; i<arr.length; i++){
+            answer[i] = arr[i];
+        }
+        
+        return answer;
+    }
+}
