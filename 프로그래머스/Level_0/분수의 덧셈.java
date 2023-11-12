c5dcda4f8cb26b957a59d83e2818b3a4e6c05d2e
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-11-13 01:08:34 +0800

    Java

--- 프로그래머스/Level_0/분수의 덧셈.java
+++ 프로그래머스/Level_0/분수의 덧셈.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     
     public int gcd(int a, int b){
@@ -15,6 +16,28 @@ class Solution {
         answer[1] = b;
         
         a = gcd(answer[0], answer[1]);
+        
+        answer[0] /= a;
+        answer[1] /= a;
+        
+        return answer;
+    }
+}
+*/
+class Solution {
+    
+    public int gcd(int a, int b){
+        if(a%b == 0) return b;
+        return gcd(b, a%b);
+    }
+    
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int[2];
+
+        answer[0] = numer1 * denom2 + numer2 * denom1;
+        answer[1] = denom2 * denom1;
+        
+        int a = gcd(answer[0], answer[1]);
         
         answer[0] /= a;
         answer[1] /= a;
