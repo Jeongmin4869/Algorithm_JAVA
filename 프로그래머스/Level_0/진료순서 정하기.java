6a2c9232a9dffa1fc10f6f66d0f11c85f4191103
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-11-12 12:50:20 +0800

    Java
    Stream

--- 프로그래머스/Level_0/진료순서 정하기.java
+++ 프로그래머스/Level_0/진료순서 정하기.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int[] emergency) {
+        /*
         int size = emergency.length;
         int[] answer = new int[size];
         int[] rank = emergency.clone();
@@ -8,10 +11,21 @@ class Solution {
         Arrays.sort(rank);
         for(int i=size-1; i>=0; i--){
             for(int j = 0; j<size; j++){
-                if(rank[i] == emergency[j]) answer[j] = size-i;
+                if(rank[i] == emergency[j]) {
+                    answer[j] = size-i;
+                    break;
+                }
             }
         }
-        
         return answer;
+        */
+        
+        return Arrays.stream(emergency)
+                .map(i -> Arrays.stream(emergency)
+                           .boxed()
+                           .sorted(Comparator.reverseOrder())
+                           .collect(Collectors.toList())
+                           .indexOf(i) + 1
+                ).toArray();
     }
 }
