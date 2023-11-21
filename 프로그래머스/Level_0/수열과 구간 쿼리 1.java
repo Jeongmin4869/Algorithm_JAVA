ec841795fd7eb65bbaeebf885b18bdbf5aa520ab
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2023-11-21 23:07:44 +0900

    Java

---
+++ 프로그래머스/Level_0/수열과 구간 쿼리 1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr.clone();
+        for(int i=0; i<queries.length; i++){
+            int s = queries[i][0], e = queries[i][1];
+            for(int j = s; j<=e; j++){
+                answer[j] += 1;
+            }
+        }
+        return answer;
+    }
+}
