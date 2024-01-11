033c39c0b5c599ad4c0e8eca81e625302b88af7a
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-01-12 00:44:33 +0900

    Java

---
+++ 프로그래머스/Level_0/홀수 vs 짝수.java
@@ -0,0 +1,17 @@
+import java.util.stream.IntStream;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        /*
+        int num1=0, num2=0;
+        for(int i=0; i<num_list.length; i++){
+            if(i%2==0)num1 += num_list[i];
+            else num2 += num_list[i];
+        }
+        return Math.max(num1, num2);
+        */
+        return Math.max( IntStream.iterate(0, i-> i<num_list.length, i -> i+2).map(i->num_list[i]).sum(), 
+                         IntStream.iterate(1, i-> i<num_list.length, i -> i+2).map(i->num_list[i]).sum());
+    }
+}
