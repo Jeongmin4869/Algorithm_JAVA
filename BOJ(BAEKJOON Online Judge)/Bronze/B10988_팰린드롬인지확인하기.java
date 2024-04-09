8784493e788e7b9ac9cb2daacada0fabe3013343
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-04-09 22:01:19 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Bronze/B10988_팰린드롬인지확인하기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main_B10988 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int len = str.length();
+        int answer = 1;
+        for(int i=0; i<len/2; i++){
+            if(str.charAt(i) != str.charAt(len-1-i)){
+                answer = 0;
+                break;
+            }
+        }
+        System.out.println(answer);
+        sc.close();
+    }
+}
