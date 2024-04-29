fa42081b5ad42b6eab7c6f2efcfa851514819397
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-04-29 09:55:50 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Bronze/B15894_수학은체육과목입니다.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main_B15894 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Long num = sc.nextLong();
+        System.out.println(num*4);
+        sc.close();
+    }
+}
