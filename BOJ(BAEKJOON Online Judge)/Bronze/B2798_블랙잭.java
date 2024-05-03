6e645c7c97d456f0493a0d909d764188c35053ca
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-05-03 20:29:59 +0900

    ㅓㅁㅍㅁ

--- BOJ(BAEKJOON Online Judge)/Bronze/B2798_블랙잭.java
+++ BOJ(BAEKJOON Online Judge)/Bronze/B2798_블랙잭.java
@@ -1,3 +1,5 @@
+// 완전탐색
+/*
 import java.util.*;
 
 public class Main_B2798 {
@@ -25,5 +27,47 @@ public class Main_B2798 {
         System.out.println(num);
         
         sc.close();
+    }
+}
+*/
+
+// DFS
+import java.util.*;
+
+public class Main {
+    private static int N, max, answer;
+    private static boolean[] visited;
+    private static int[] arr;
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        max = sc.nextInt();
+        arr = new int[N];
+        visited = new boolean[N];
+        
+        for(int i=0; i<N; i++){
+            int num = sc.nextInt();
+            arr[i] = num;
+        }
+        
+        dfs(0, 0);
+        System.out.println(answer);
+        sc.close();
+    }
+    
+    private static void dfs(int sum, int cnt){      
+        if(cnt >= 3){
+            if(sum <= max) answer = Math.max(answer,sum);
+            return;
+        }
+        
+        for(int i=0; i<N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(sum+arr[i], cnt+1);
+                visited[i] = false;
+            }
+        }
     }
 }
