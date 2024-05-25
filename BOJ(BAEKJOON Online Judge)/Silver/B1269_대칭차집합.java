f47c734e7951e20a7f94c35045e59ee2843c44db
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-05-25 23:00:36 +0900

    ㅓㅁㅍㅁ

--- BOJ(BAEKJOON Online Judge)/Silver/B1269_대칭차집합.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B1269_대칭차집합.java
@@ -32,5 +32,47 @@ public class Main_B1269 {
         
     }
 
-    
+}
+
+
+/* HashSet 사용 */
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 두 집합의 크기 입력
+        int N1 = sc.nextInt();
+        int N2 = sc.nextInt();
+        
+        // 첫 번째 집합 입력
+        Set<Integer> set1 = new HashSet<>();
+        for (int i = 0; i < N1; i++) {
+            set1.add(sc.nextInt());
+        }
+        
+        // 두 번째 집합 입력
+        Set<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < N2; i++) {
+            set2.add(sc.nextInt());
+        }
+        
+        // 합집합 구하기
+        Set<Integer> allSet = new HashSet<>(set1);
+        allSet.addAll(set2);
+        
+        // 교집합 구하기
+        Set<Integer> temp = new HashSet<>(set1);
+        temp.retainAll(set2);
+        
+        // 합집합에서 교집합 요소 제거하기 (대칭 차집합 구하기)
+        allSet.removeAll(temp);
+        
+        // 결과 출력
+        System.out.println(allSet.size());
+        
+        sc.close();
+    }
 }
