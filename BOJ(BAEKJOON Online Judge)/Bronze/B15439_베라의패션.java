505101ff581d3834d171ce72a84991f78eed00ed
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-06-29 00:18:50 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Bronze/B15439_베라의패션.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main_B15439 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N*(N-1));
+        sc.close();
+    }
+
+}
