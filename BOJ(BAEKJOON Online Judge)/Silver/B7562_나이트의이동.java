fcbb479006bdedb6277696751e9b56d55003debc
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-10-27 17:09:45 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B7562_나이트의이동.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B7562_나이트의이동.java
@@ -2,71 +2,93 @@ import java.util.*;
 import java.io.*;
 
 class Main_B7562 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        
         for(int i=0; i<N; i++){
-            int l = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int result = bfs(l, x1, y1, x2, y2);
+            int I = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+            int sx = Integer.parseInt(st.nextToken());
+            int sy = Integer.parseInt(st.nextToken());
+            
+            st = new StringTokenizer(br.readLine());
+            int ex = Integer.parseInt(st.nextToken());
+            int ey = Integer.parseInt(st.nextToken());
+
+            int result = bfs(I, sx, sy, ex, ey);
             sb.append(result).append("\n");
         }
-        System.out.println(sb); 
+        System.out.println(sb);
     }
 
-    public static int bfs(int l, int sx, int sy, int ex, int ey){
+    public static int bfs(int M, int sx, int sy, int ex, int ey){
+        int count = 0;
+        int[][] map = new int[M][M];
         Queue<int[]> q = new LinkedList<>();
-        int[][] map = new int[l][l];
         q.offer(new int[] {sx, sy});
+
+        int[] dx = {2, 2, -2,-2, 1, 1, -1, -1};
+        int[] dy = {1,-1, -1, 1, 2,-2,  2, -2};
+        
         while(!q.isEmpty()){
-           int[] now = q.poll();
+            int[] now = q.poll();
             int xx = now[0];
             int yy = now[1];
-            if(xx==ex && yy==ey) return map[xx][yy];
-            if(xx+2>=0 && xx+2<l && yy+1 >=0 && yy+1<l && map[xx+2][yy+1]==0){
-                map[xx+2][yy+1] = map[xx][yy]+1;
-                q.offer(new int[] {xx+2, yy+1});
+            
+            if(xx == ex && yy == ey) return map[xx][yy];
+
+            for(int i=0; i<8; i++){
+                int nx = xx+dx[i];
+                int ny = yy+dy[i];
+                if(nx >= 0 && nx < M && ny >=0 && ny < M && map[nx][ny] == 0){
+                    map[nx][ny] = map[xx][yy]+1;
+                    q.offer(new int[] {nx,ny});
+                }
             }
-            if(xx+2>=0 && xx+2<l && yy-1 >=0 && yy-1<l && map[xx+2][yy-1]==0){
-                map[xx+2][yy-1] = map[xx][yy]+1;
-                q.offer(new int[] {xx+2, yy-1});
-            }
-            if(xx+1>=0 && xx+1<l && yy+2 >=0 && yy+2<l && map[xx+1][yy+2]==0){
+
+            /*
+            if(xx+1 >= 0 && xx+1 < M && yy+2 >=0 && yy+2 < M && map[xx+1][yy+2] == 0){
                 map[xx+1][yy+2] = map[xx][yy]+1;
                 q.offer(new int[] {xx+1, yy+2});
             }
-            if(xx-1>=0 && xx-1<l && yy+2 >=0 && yy+2<l && map[xx-1][yy+2]==0){
+            if(xx-1 >= 0 && xx-1 < M && yy+2 >=0 && yy+2 < M && map[xx-1][yy+2] == 0){
                 map[xx-1][yy+2] = map[xx][yy]+1;
                 q.offer(new int[] {xx-1, yy+2});
             }
-            
-            if(xx-2>=0 && xx-2<l && yy+1 >=0 && yy+1<l && map[xx-2][yy+1]==0){
-                map[xx-2][yy+1] = map[xx][yy]+1;
-                q.offer(new int[] {xx-2, yy+1});
-            }
-            if(xx-2>=0 && xx-2<l && yy-1 >=0 && yy-1<l && map[xx-2][yy-1]==0){
-                map[xx-2][yy-1] = map[xx][yy]+1;
-                q.offer(new int[] {xx-2, yy-1});
-            }
-            if(xx+1>=0 && xx+1<l && yy-2 >=0 && yy-2<l && map[xx+1][yy-2]==0){
+            if(xx+1 >= 0 && xx+1 < M && yy-2 >=0 && yy-2 < M && map[xx+1][yy-2] == 0){
                 map[xx+1][yy-2] = map[xx][yy]+1;
                 q.offer(new int[] {xx+1, yy-2});
             }
-            if(xx-1>=0 && xx-1<l && yy-2 >=0 && yy-2<l && map[xx-1][yy-2]==0){
+            if(xx-1 >= 0 && xx-1 < M && yy-2 >=0 && yy-2 < M && map[xx-1][yy-2] == 0){
                 map[xx-1][yy-2] = map[xx][yy]+1;
                 q.offer(new int[] {xx-1, yy-2});
             }
+
             
-            
+            if(xx+2 >= 0 && xx+2 < M && yy+1 >=0 && yy+1 < M && map[xx+2][yy+1] == 0){
+                map[xx+2][yy+1] = map[xx][yy]+1;
+                q.offer(new int[] {xx+2, yy+1});
+            }
+            if(xx-2 >= 0 && xx-2 < M && yy+1 >=0 && yy+1 < M && map[xx-2][yy+1] == 0){
+                map[xx-2][yy+1] = map[xx][yy]+1;
+                q.offer(new int[] {xx-2, yy+1});
+            }
+            if(xx+2 >= 0 && xx+2 < M && yy-1 >=0 && yy-1 < M && map[xx+2][yy-1] == 0){
+                map[xx+2][yy-1] = map[xx][yy]+1;
+                q.offer(new int[] {xx+2, yy-1});
+            }
+            if(xx-2 >= 0 && xx-2 < M && yy-1 >=0 && yy-1 < M && map[xx-2][yy-1] == 0){
+                map[xx-2][yy-1] = map[xx][yy]+1;
+                q.offer(new int[] {xx-2, yy-1});
+            }
+            */
         }
         return -1;
     }
-    
 }
