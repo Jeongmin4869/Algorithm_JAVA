f1e9d414efba689f2db9cafcadbd0b94d4c13fa7
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-11-18 21:59:59 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B1268_임시반장정하기.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B1268_임시반장정하기.java
@@ -41,3 +41,47 @@ class Main_B1268 {
         System.out.println(keys.get(0));
     }
 }
+
+
+/*
+
+2. Stack 활용 -> 메모리를 더 많이 사용함
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] classes = new int[n+1][5+1];
+        for(int i=1; i<=n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=5; j++){
+                classes[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int max = 0;
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            Set<Integer> student = new HashSet<>();
+            for (int year = 1; year <= 5; year++) {
+                for (int j = 1; j <= n; j++) {
+                    if(i==j) continue;
+                    if (classes[i][year] == classes[j][year]) {
+                        student.add(j);
+                    }
+                }
+            }
+            if(student.size()>max){
+                max = student.size();
+                result = i;
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
+
+*/
