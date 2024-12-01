eaa86debdbd9c1d592649c840f9590a890046223
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-12-01 20:35:39 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B1730_판화.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B1730_판화.java
@@ -6,10 +6,10 @@ class Main_B1730 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
-        char[][] arr = new char[n][n];
-        
-        // 격자를 '.'으로 초기화
-        for (int i = 0; i < n; i++) Arrays.fill(arr[i], '.');
+
+        // 수평, 수직 이동 상태 기록 배열
+        boolean[][] horizontal = new boolean[n][n];
+        boolean[][] vertical = new boolean[n][n];
         
         String move = br.readLine();
         int x = 0, y = 0; // 시작 위치
@@ -17,30 +17,44 @@ class Main_B1730 {
         for (int i = 0; i < move.length(); i++) {
             char c = move.charAt(i);
 
-            int nx = x, ny = y; 
+            int nx = x, ny = y; // 다음 위치
             if (c == 'U') nx -= 1;
             if (c == 'D') nx += 1;
             if (c == 'L') ny -= 1;
             if (c == 'R') ny += 1;
 
-            // 격자 범위를 벗어나면 무시
             if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
 
             if (c == 'U' || c == 'D') {
-                arr[x][y] = (arr[x][y] == '-') ? '+' : '|';
-                arr[nx][ny] = (arr[nx][ny] == '-') ? '+' : '|';
+                vertical[x][y] = true;
+                vertical[nx][ny] = true;
             } else if (c == 'L' || c == 'R') {
-                arr[x][y] = (arr[x][y] == '|') ? '+' : '-';
-                arr[nx][ny] = (arr[nx][ny] == '|') ? '+' : '-';
+                horizontal[x][y] = true;
+                horizontal[nx][ny] = true;
             }
 
             x = nx;
             y = ny; // 다음 위치로 이동
         }
 
+        // 출력
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(arr[i]).append("\n");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(vertical[i][j] && horizontal[i][j]){
+                    sb.append("+");
+                }
+                else if(vertical[i][j]){
+                    sb.append("|");
+                }
+                else if(horizontal[i][j]){
+                    sb.append("-");
+                }
+                else {
+                    sb.append(".");
+                }
+            }
+            sb.append("\n");
         }
         System.out.print(sb);
     }
