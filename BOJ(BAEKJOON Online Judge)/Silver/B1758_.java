943283d132145f7d80e87ab311a7c3921852ccfd
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2024-12-21 16:55:28 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Silver/B1758_.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+  
+class Main_B1758 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Integer[] tips = new Integer[N];
+        for(int i=0; i<N; i++){
+            tips[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(tips, Collections.reverseOrder());
+
+        long result = 0;
+        for(int i=0; i<N; i++){
+            int now = tips[i] - i;
+            if(now<0) break;
+            result += now;
+        }
+
+        System.out.println(result);
+    }
+}
