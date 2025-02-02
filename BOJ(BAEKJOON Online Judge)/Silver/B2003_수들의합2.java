03683f1a8a2d41208d93346ab841ca29500bcba0
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-02-02 19:39:09 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2003_수들의합2.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2003_수들의합2.java
@@ -1,3 +1,7 @@
+/*    
+
+// 투포인터 -> 투포인터는 정렬된 배열에 사용하는것이 좋다 
+
 import java.util.*;
 import java.io.*;
 
@@ -29,6 +33,37 @@ class Main_B2003 {
             if(sum == M){
                 count += 1;               
             }            
+        }
+        System.out.println(count);
+    } 
+}
+
+*/
+
+// 누적합 -> 정렬되지 않은 배열
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<i; j++){
+                if((arr[i] - arr[j]) == M) count += 1;
+            }
         }
         System.out.println(count);
     } 
