d1f7bafa7a27012d57fd8d82b1c56304bd24d72f
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-02-11 21:08:57 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2303_숫자게임.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2303_숫자게임.java
@@ -1,14 +1,12 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main_B2303 {
+class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int index = 0;
-        int maxval = 0;
+        int maxval = -1;
         StringTokenizer st ;
         for(int i=0; i<N; i++){
             int[] arr = new int[5];
@@ -16,30 +14,28 @@ class Main_B2303 {
             for(int j=0; j<5; j++){
                 arr[j] = Integer.parseInt(st.nextToken());
             }
-            Arrays.sort(arr);
             int now = check(arr);
-            if((now%10 > maxval%10 )|| ((now%10 == maxval%10) && (now>maxval))){
+            if(now >= maxval){
                 index = i+1;
                 maxval = now;
-            }
-            
+            }            
         }
         
         System.out.println(index);
     }
 
     public static int check(int arr[]){
-        int max = 0;
+        int maxval = 0;
         for(int i=0; i<3; i++){
             for(int j=i+1; j<4; j++){
                 for(int k=j+1; k<5; k++){
-                    int now = arr[i] + arr[j] + arr[k];
-                    if(max%10 < now%10) max = now;
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if(sum%10 > maxval){
+                        maxval = sum%10;
+                    }
                 }
             }
         }
-        return max;
+        return maxval;
     }
-
-    
 }
