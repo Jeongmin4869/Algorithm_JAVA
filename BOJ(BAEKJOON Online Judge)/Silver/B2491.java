18d6b13465d13131d2074ec27b6ff08c5f0c9023
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-02-28 21:02:58 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2491.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2491.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main_B2491 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,33 +13,28 @@ class Main_B2491 {
         }
         
         int[] a = new int[N];
+        Arrays.fill(a,1);
         int fre = arr[0];
         for(int i=1; i<N; i++){
-           if(fre < arr[i]){
+           if(fre <= arr[i]){
                a[i] = a[i-1]+1;
-               fre = a[i];
+               
            }
-            else{
-                a[i] = a[i-1];
-            }
+            fre = arr[i];
         }
 
         int[] b = new int[N];
+        Arrays.fill(b,1);
         fre = arr[N-1];
         for(int i=N-2; i>=0; i--){
-           if(fre < arr[i]){
+           if(fre <= arr[i]){
                b[i] = b[i+1]+1;
-               fre = b[i];
            }
-            else{
-                b[i] = b[i+1];
-            }
+            fre = arr[i];
         }
 
-        int max = 0;
-        for(int i=0; i<N; i++){
-            max = Math.max(max, a[i] + b[i]);
-        }
-        System.out.println(max);
+        Arrays.sort(a);
+        Arrays.sort(b);
+        System.out.println(Math.max(a[N-1], b[N-1]));
     }
 }
