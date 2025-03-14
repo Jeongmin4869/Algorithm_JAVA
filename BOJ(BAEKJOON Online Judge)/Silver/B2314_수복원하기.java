256d961c6104164bcf84062a78706c37750a4d2d
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-03-14 23:04:18 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2314_수복원하기.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2314_수복원하기.java
@@ -1,35 +1,33 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main {
+class Main_B2312 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         boolean[] arr = new boolean[100001];
         for(int i=2; i<=100000; i++){
-
+            if(!arr[i]){
                 for(int j=2; j*i<=100000; j++){
                     arr[j*i] = true;
                 }
-
+            }
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i=0; i<N; i++){
             int num = Integer.parseInt(br.readLine());
             int now = num;
-            for(int j=2; j<=num && !arr[j]; j++){
-                System.out.println(now + " " + j);
-                System.out.println(now%j);
+            for(int j=2; j<=num ; j++){
+                
+                
                 if(now%j==0){
                     int count = 0;
                     while(now%j==0){
                         now /= j;
                         count += 1;
                     }
-                    sb.append(j + " "+ count).append("\n");
+                    sb.append(j + " " + count).append("\n");
                 }
                 if(now <= 1){
                     break;
