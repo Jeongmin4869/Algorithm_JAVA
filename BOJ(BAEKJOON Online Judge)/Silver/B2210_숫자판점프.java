87c4a356ffa4b3d546e48cb4cd41853435d2de41
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-03-15 21:24:55 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2210_숫자판점프.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2210_숫자판점프.java
@@ -2,12 +2,22 @@ import java.util.*;
 import java.io.*;
 
 class Main_B2210 {
-
-    static boolean[][] visited;
+    
     static String[][] arr;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
     static Set<String> s;
+
+    static class Node{
+        int x, y;
+        String str;
+        Node (int x, int y, String str){
+            this.x = x;
+            this.y = y;
+            this.str = str;
+        }
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         arr = new String[5][5];
@@ -17,10 +27,9 @@ class Main_B2210 {
             arr[i] = br.readLine().split(" ");
         }
 
-        visited = new boolean[5][5];
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                dfs(i, j, arr[i][j]);
+                bfs(i, j, arr[i][j]);
             }
         }
 
@@ -28,6 +37,34 @@ class Main_B2210 {
         System.out.println(s.size());
     }
 
+    public static void bfs(int x, int y, String str){
+        Queue<Node> q = new LinkedList<>();
+        Node node = new Node(x, y, str);
+        q.offer(node);
+        
+        while(!q.isEmpty()){
+            Node now = q.poll();
+            int nx = now.x;
+            int ny = now.y;
+            String nStr = now.str;
+
+            if(nStr.length() == 6){
+                s.add(nStr);
+                continue;
+            }
+            
+            for(int i=0; i<4; i++){
+                int xx = nx + dx[i];
+                int yy = ny + dy[i];
+                if(xx >= 0 && xx < 5 && yy >= 0 && yy < 5){
+                    q.offer(new Node(xx, yy, nStr + arr[xx][yy]));                    
+                }
+                
+            }
+        }        
+    }
+
+    
     public static void dfs(int x, int y, String str){
         if(str.length() == 6){
             s.add(str);
@@ -37,12 +74,8 @@ class Main_B2210 {
         for(int i=0; i<4; i++){
             int px = dx[i] + x;
             int py = dy[i] + y;
-            if(px>=0 && px<5 && py>=0 && py<5){
-                //if(!visited[px][py]){
-                //    visited[px][py] = true;
-                    dfs(px, py, str+arr[px][py]);
-                //    visited[px][py] = false;
-                //}
+            if(px>=0 && px<5 && py>=0 && py<5){                
+                dfs(px, py, str+arr[px][py]);                
             }
         }
         
