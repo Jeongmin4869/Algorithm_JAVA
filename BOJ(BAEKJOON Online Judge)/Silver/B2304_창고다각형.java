ddcc3943340492cd98fb2202472ab0e2ae821670
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-03-22 20:59:08 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2304_창고다각형.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2304_창고다각형.java
@@ -1,9 +1,7 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main {
+class Main_B2304 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n =Integer.parseInt(br.readLine());
@@ -15,11 +13,13 @@ class Main {
             int b =Integer.parseInt(st.nextToken());
             list.add(new int[]{a,b});
         }
-        
+
+        // x 좌표 기준 정렬
         Collections.sort(list, (o1, o2) -> {
             return o1[0] - o2[0];
         });
 
+        // max값과 해당 값의 x좌표 찾기 (동일값이 있는 경우 x값이 큰 값으로 셋팅)
         int maxlen = 0;
         int maxidx = 0;
         for(int i=0; i<n; i++){
@@ -28,7 +28,8 @@ class Main {
                 maxidx = i;
             }
         }
-        
+
+        // 왼쪽 면적
         int total = 0;
         int nowmax = 0;
         int s = 0;
@@ -42,7 +43,8 @@ class Main {
             }            
         }
 
-        
+
+        // 오른쪽 면적
         nowmax = 0;
         s = list.get(n-1)[0];
         for(int i=n-1; i>=maxidx; i--){
@@ -54,7 +56,8 @@ class Main {
                 nowmax = nowh;
             }            
         }
-        
+
+        // 기준이 되었던 max값
         total += maxlen;
                 
         System.out.println(total);
