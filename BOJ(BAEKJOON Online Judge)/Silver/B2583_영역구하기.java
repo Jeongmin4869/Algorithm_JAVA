c50e48823a6ae2210afad11a480305b2bf6363be
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-06-02 21:51:24 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B2583_영역구하기.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B2583_영역구하기.java
@@ -2,10 +2,10 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    
-    static boolean[][] area;
-    static int count, total;
-    static int M, N;
+
+    static boolean[][] arr;
+    static int count;
+    static int N, M;
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
     
@@ -16,7 +16,7 @@ class Main {
         N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        area = new boolean[M][N];
+        arr = new boolean[M][N];
         for(int i=0; i<K; i++){
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
@@ -24,21 +24,19 @@ class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
             for(int j=y1; j<y2; j++){
-                for(int k=x1; k<x2; k++){                        
-                        area[j][k] = true;
+                for(int k=x1; k<x2; k++){
+                    arr[j][k] = true;
                 }
             }
         }
 
-        total = 0;
         StringBuilder sb = new StringBuilder();
         List<Integer> list = new ArrayList<>();
         for(int i=0; i<M; i++){
             for(int j=0; j<N; j++){
-                if(!area[i][j]){                    
-                    count = 1;                    
-                    total += 1;                                        
-                    list.add(dfs(i, j));                    
+                if(!arr[i][j]){
+                    count = 1;
+                    list.add(bfs(i, j));
                 }
             }
         }
@@ -48,19 +46,41 @@ class Main {
             sb.append(list.get(i) + " ");
         }
         
-        System.out.println(total);
+        System.out.println(list.size());
         System.out.print(sb);
     }
 
-    public static int dfs(int x, int y){    
-        area[x][y] = true;
+    public static int dfs(int x, int y){
+
+        arr[x][y] = true;
         for(int i=0; i<4; i++){
             int xx = dx[i] + x;
             int yy = dy[i] + y;
-            if(xx>=0 && xx<M && yy>=0 && yy <N && !area[xx][yy]){
+            if(xx>=0 && xx<M && yy>=0 && yy <N && !arr[xx][yy]){
                 count += 1;
-                area[xx][yy] = true;
                 dfs(xx, yy);
+            }
+        }
+        return count;
+    }
+
+    public static int bfs(int x, int y){
+        Queue<int[]> q = new LinkedList<>();
+        arr[x][y] = true;
+        q.add(new int[]{x,y});
+        
+        while(!q.isEmpty()){
+            int[] top = q.poll();
+            int px = top[0];
+            int py = top[1];
+            for(int i=0; i<4; i++){
+                int xx = px + dx[i];
+                int yy = py + dy[i];
+                if(xx>=0 && xx<M && yy>=0 && yy<N && !arr[xx][yy]){
+                    q.offer(new int[] {xx,yy});
+                    count += 1;
+                    arr[xx][yy] = true;
+                }
             }
         }
         return count;
