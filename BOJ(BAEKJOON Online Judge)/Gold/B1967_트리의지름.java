01c059f0206afadbf711a036737e239fb67f98ab
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-07-17 20:55:17 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Gold/B1967_트리의지름.java
+++ BOJ(BAEKJOON Online Judge)/Gold/B1967_트리의지름.java
@@ -5,7 +5,8 @@ import java.io.*;
 // The main method must be in a class named "Main".
 class Main {
     static ArrayList<ArrayList<Node>> list;
-    static int[][] arr;
+    static boolean visited[];
+    static int farthestIdx = 0;
     static int max = 0;
 
     static class Node{
@@ -21,14 +22,14 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        arr = new int[N+1][N+1];
+        list = new ArrayList<>();
         
         for(int i=0; i<=N; i++){
             list.add(new ArrayList<>());
         }
         
         StringTokenizer st ;
-        for(int i=0; i<N; i++){
+        for(int i=0; i<N-1; i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
@@ -37,15 +38,38 @@ class Main {
             list.get(v).add(new Node(u, m));
             
         }
+/*
+        for(int i=1; i<=N; i++){
+            visited = new boolean[N+1];
+            visited[i] = true;
+            dfs(i, 0);
+        }
+*/
 
-        int result = dfs(1, 0);
-        System.out.println(result);
+        visited = new boolean[N+1];
+        visited[1] = true;
+        dfs(1, 0);
+
+        visited = new boolean[N+1];
+        visited[farthestIdx] = true;
+        max = 0;
+        dfs(farthestIdx, 0);
+        System.out.println(max);
     }
 
     public static void dfs(int node, int sum){
         for(int i=0; i<list.get(node).size(); i++){
-            int newnode = list.get(node).get(i);
-            dfs(newnode, sum + list.get(newnode).weight);
+            Node newnode = list.get(node).get(i);
+            if(!visited[newnode.n]){
+                visited[newnode.n] = true;
+                dfs(newnode.n, sum + newnode.weight);
+                visited[newnode.n] = false;
+            }
         }
+        if(sum>max){
+            max = sum;
+            farthestIdx = node;
+        }
+        
     }
 }
