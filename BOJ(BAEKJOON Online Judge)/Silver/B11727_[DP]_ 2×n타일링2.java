3f1ddcfebc19d47f3d42f473beb31b944b127ecc
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-08-24 22:32:20 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Silver/B11727_[DP]_ 2×n타일링2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 3;
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-2]*2+ dp[i-1])%10007;
+        }
+        
+        
+        System.out.println(dp[n]);
+    }
+}
