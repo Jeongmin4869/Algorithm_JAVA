89e7956cf3a51d7fdaa7512cd89f91db1bcf05e5
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-09-05 22:31:09 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B1965_[DP/LIS]_상자넣기.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B1965_[DP/LIS]_상자넣기.java
@@ -15,15 +15,18 @@ class Main {
             dp[i] = 1;
         }
 
-        
-        
+        // LIS 최장증가부분수열을 저장할 변수
+        int answer = 0;
         for(int i=0; i<N; i++){
-            for(int j=0; j<=i; j++ ){
+            // i번째 상자까지의 LIS을 구하기 위한 반복문
+            for(int j=0; j<i; j++ ){
                 if(arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j]+1);
             }
+            // 현재까지 구한 LIS값을 항상 answer에 저장
+            answer= Math.max(answer, dp[i]);
         }
 
         
-        System.out.println(dp[N-1]);
+        System.out.println(answer);
     }
 }
