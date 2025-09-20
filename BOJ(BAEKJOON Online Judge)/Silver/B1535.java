827582bf95cb563f6cee620f17c15db7b85dffb9
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-09-20 21:29:53 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Silver/B1535.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] happy = new int[N+1];
+        int[] health = new int[N+1];
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            health[i] = Integer.parseInt(st1.nextToken());
+            happy[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        int[]dp = new int[101];
+        int result = 0;
+        for(int i=1; i<=N; i++){
+            for(int j=99; j>=0; j--){
+                if(j-health[i]>0){
+                    dp[j] = Math.max(dp[j], dp[j-health[i]]+happy[i]);
+                    result = Math.max(dp[j], result);
+                }
+            }
+                
+        }
+        System.out.println(result);
+    }
+}
