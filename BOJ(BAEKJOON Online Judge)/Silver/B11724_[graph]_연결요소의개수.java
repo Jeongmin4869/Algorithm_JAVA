69be3dfbaf184b3543fd740623fc78adfbfa6cf2
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-09-27 22:38:13 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Silver/B11724_[graph]_연결요소의개수.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int N;
+    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N+1];
+        list = new ArrayList<>();
+        for(int i=0; i<=N; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list.get(u).add(v);
+            list.get(v).add(u);
+        }
+
+        int count = 0;
+        for(int i=1; i<=N; i++){
+            if(!visited[i]){
+                count += 1;
+                dfs(i);
+            }
+        }
+        System.out.print(count);
+    }
+
+    public static void dfs(int node){
+        visited[node] = true;
+        for(int i=0; i<list.get(node).size(); i++){
+            int newnode = list.get(node).get(i);
+            if(!visited[newnode]){
+                dfs(list.get(node).get(i));
+            }
+        }
+    }
+}
