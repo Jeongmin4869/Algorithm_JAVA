f579ace0ba96428b58e683c10f219b28134e9265
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-11-05 20:15:17 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B1446_[DP]_지름길.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B1446_[DP]_지름길.java
@@ -4,6 +4,16 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
+    static class Pos{
+        int s;
+        int e; 
+        int l;
+        Pos(int s, int e, int l){
+            this.s=s;
+            this.e = e;
+            this.l = l;
+        }
+    }
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,22 +25,30 @@ class Main {
         for(int i=1; i<=D; i++){
             dp[i] = i;
         }
-        
+
+        List<Pos> list = new ArrayList<>();
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int l = Integer.parseInt(st.nextToken());
             if(e>D) continue;
-            if(e-s<l) continue;
-            dp[e] = Math.min(dp[e], dp[s]+l);
+            if(e-s<=l) continue;
+            list.add(new Pos(s, e, l));
         }
 
-        int min = dp[D];
-        for(int i=1; i<=D; i++){
-            min = Math.min(dp[i] + D-i, min);
+        Collections.sort(list, (o1, o2)->{
+            return o1.e - o2.e;
+        });
+
+        for(int i=0; i<list.size(); i++){
+            Pos pos = list.get(i);
+            dp[pos.e] = Math.min(dp[pos.e], dp[pos.s]+pos.l);
+            for(int j = pos.e+1; j<=D; j++){
+                dp[j] = Math.min(dp[j-1]+1, dp[j]);
+            }
         }
         
-        System.out.println(min);
+        System.out.println(dp[D]);
     }
 }
