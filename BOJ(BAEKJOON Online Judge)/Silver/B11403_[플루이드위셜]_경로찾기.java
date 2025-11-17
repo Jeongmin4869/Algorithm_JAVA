8182f04d4dc5b50a625cc3bed0bb02048ccf29fa
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-11-17 20:40:05 +0900

    Java

---
+++ BOJ(BAEKJOON Online Judge)/Silver/B11403_[플루이드위셜]_경로찾기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[][] map = new char[N][N];
+        for(int i=0; i<N; i++){
+            String str = br.readLine().replace(" ", "");
+            for(int j=0; j<N; j++){
+                map[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int k=0; k<N; k++){
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(map[i][k]=='1' && map[k][j] == '1'){
+                        map[i][j] = '1';
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb= new StringBuilder();
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                sb.append(map[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
