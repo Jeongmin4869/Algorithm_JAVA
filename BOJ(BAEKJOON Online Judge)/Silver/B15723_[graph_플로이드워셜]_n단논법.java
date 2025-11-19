a582b3f91c89e3256b347bc9428f9dcefa0de895
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-11-19 22:16:05 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B15723_[graph_플로이드워셜]_n단논법.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B15723_[graph_플로이드워셜]_n단논법.java
@@ -15,21 +15,23 @@ class Main {
             arr[s][e] = 1;
         }
 
+        for(int k=0; k<26; k++){
+            for(int i=0; i<26; i++){
+                for(int j=0; j<26; j++){
+                    if(arr[i][k]==1 && arr[k][j]==1){
+                        arr[i][j] = 1;
+                    }
+                }
+            }
+        }
+
         int T = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++){
+        for(int i=0; i<T; i++){
             String str = br.readLine();
             int s = str.charAt(0)-'a';
             int e = str.charAt(str.length()-1)-'a';
-            if(check(s, e, arr)) System.out.println("T");
+            if(arr[s][e]==1) System.out.println("T");
             else System.out.println("F");
         }
-        
-    }
-
-    public static boolean check(int s, int e, int[][]arr){
-        for(int k=0; k<arr[0].length; k++){
-            if(arr[s][k]==1 && arr[k][e]==1) return true;
-        }
-        return true;
     }
 }
