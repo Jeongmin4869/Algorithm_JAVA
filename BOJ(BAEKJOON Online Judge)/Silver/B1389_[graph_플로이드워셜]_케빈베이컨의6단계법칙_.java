26df36e1734472ae89136a7bc8659ce2312088a8
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2025-11-21 15:56:04 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Silver/B1389_[graph_플로이드워셜]_케빈베이컨의6단계법칙_.java
+++ BOJ(BAEKJOON Online Judge)/Silver/B1389_[graph_플로이드워셜]_케빈베이컨의6단계법칙_.java
@@ -4,43 +4,43 @@ import java.io.*;
 
 // The main method must be in a class named "Main".
 class Main {
-    static int INF = Integer.MAX_VALUE;
+    static int INF = 1000000000;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][N];
-        for(int i=0; i<N; i++){
+        int[][] arr = new int[N+1][N+1];
+        for(int i=1; i<=N; i++){
             Arrays.fill(arr[i], INF);
             arr[i][i] = 0;
         }
 
         for(int i=0; i<M; i++){
-            String str = br.readLine();
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken())-'A';
-            int b = Integer.parseInt(st.nextToken())-'A';
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             arr[a][b] = 1;
+            arr[b][a] = 1;
         }
 
-        for(int k=0; k<N; k++){
-            for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
+        for(int k=1; k<=N; k++){
+            for(int i=1; i<=N; i++){
+                for(int j=1; j<=N; j++){
                     arr[i][j] = Math.min(arr[i][j], arr[i][k]+arr[k][j]);
                 }
             }
         }
 
         int num = 0;
-        int maxsum = 0;
-        for(int i=0; i<N; i++){
+        int minsum = INF;
+        for(int i=1; i<=N; i++){
             int sum = 0;
-            for(int j=0; j<N; j++){
-                if(arr[i][j]<=6) sum += 1;
+            for(int j=1; j<=N; j++){
+                if(arr[i][j] != INF) sum += arr[i][j];
             }
-           if(maxsum<sum){
-               maxsum = sum;
+           if(minsum>sum){
+               minsum = sum;
                num = i;
            }
         }
