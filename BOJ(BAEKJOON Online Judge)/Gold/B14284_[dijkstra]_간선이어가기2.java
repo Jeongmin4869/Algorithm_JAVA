20cc1751195f6119eb90811f303a074e3a800abc
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-03-11 21:10:12 +0900

    Java

--- BOJ(BAEKJOON Online Judge)/Gold/B14284_[dijkstra]_간선이어가기2.java
+++ BOJ(BAEKJOON Online Judge)/Gold/B14284_[dijkstra]_간선이어가기2.java
@@ -36,6 +36,7 @@ class Main {
             int v = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
             g.get(u).add(new Node(v,c));
+            g.get(v).add(new Node(u,c));
         }
 
         st = new StringTokenizer(br.readLine());
@@ -52,20 +53,21 @@ class Main {
         int INF = Integer.MAX_VALUE;
         int[] dist = new int[N+1];
         Arrays.fill(dist, INF);
+        dist[s] = 0;
         PriorityQueue<Node> pq=new PriorityQueue<>();
         pq.offer(new Node(s,0));
         while(!pq.isEmpty()){
             Node top = pq.poll();
-            if(dist[top.to]<=top.c) continue;
+            if(dist[top.to]<top.c) continue;
             for(Node newnode : g.get(top.to)){
                 int cost = dist[top.to]+newnode.c;
-                if(dist[newnode.to]<=cost)
+                if(dist[newnode.to]>=cost){
+                    dist[newnode.to] = cost;
                     pq.offer(new Node(newnode.to, cost));
+                }
             }
         }
-
         return dist;
-
             
     }
 }
