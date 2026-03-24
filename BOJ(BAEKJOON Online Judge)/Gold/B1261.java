e32b358ea46f1859db5645157997c0e576d51acf
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-03-24 22:49:09 +0900

    Update B1261.java

--- BOJ(BAEKJOON Online Judge)/Gold/B1261.java
+++ BOJ(BAEKJOON Online Judge)/Gold/B1261.java
@@ -10,9 +10,10 @@ class Main {
     static int[][] map;
     static ArrayList<ArrayList>> g;
     static class Node implements Comparable<Node>{
-        int to, c;
-        Node(int to, int c){
-            this.to = to;
+        int x,y, c;
+        Node(int x, int y, int c){
+            this.x = x;
+            this.y = y;
             this.c =c;
         }
 
@@ -26,18 +27,35 @@ class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        map = new int[N+1][M+1];
 
-        for(int i=0; i<N; i++){
+        for(int i=1; i<=N; i++){
             String str = br.readLine(br.readLine());
-            for(int j=0; j<M; j++){
+            for(int j=1; j<=M; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
+        int[][] dist = dijkstra();
 
-        
+        System.out.print(dist[N][M])
     }
 
-    public int
+    public static int[][] dijkstra(){
+        int[][] dist = new int[N+1][M+1];
+        int INF = Integer.MAX_VALUE;
+        for(int i=0; i<=N; i++){
+            Arrays.fill(dist[i], INF);
+        }
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        
+
+            
+
+
+
+            
+
+    }
 }
