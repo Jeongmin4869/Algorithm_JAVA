06817860442642b9f3d4033df4dabdad4dea9500
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-06-25 22:10:07 +0900

    Java

---
+++ 프로그래머스/Level3/N으로표현.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        Set<Integer>[] dp = new HashSet[9];
+        int answer = 0;
+        for(int i=1; i<=8; i++){
+            dp[i] = new HashSet<>();
+            int num = 0;
+            for(int j=0; j<i; j++){
+                num = num*10 + N;
+            }            
+            dp[i].add(num);
+                    
+            for(int a:dp[i]){
+                for(int b:dp[i]){
+                    dp[i].add(a+b);
+                    dp[i].add(a-b);
+                    dp[i].add(a/b);
+                    dp[i].add(a*b);
+                }
+            }
+        }
+                
+        return answer;
+    }
+}
