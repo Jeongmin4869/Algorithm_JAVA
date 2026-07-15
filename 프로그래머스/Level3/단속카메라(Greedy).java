d0ba5a5b7b6808306c71cd3337d5e0ddcd81dd3f
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-07-15 22:34:11 +0900

    Java

---
+++ 프로그래머스/Level3/단속카메라(Greedy).java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        
+        Arrays.sort(routes, (o1, o2) -> {
+           return o1[1] - o2[1]; 
+        });
+        
+        int now = routes[0][1];
+        for(int i=0; i<routes.length; i++){
+            if(now <= routes[i][1] && now >= routes[i][0])
+                continue;
+            answer += 1; 
+            now = routes[i][1];
+        }
+        
+        return answer;
+        
+        
+    }
+}
