e7b416a6e705c089d5777961320dfbf48585e12a
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-07-17 22:26:13 +0900

    Java

--- 프로그래머스/Level3/베스트앨범.java
+++ 프로그래머스/Level3/베스트앨범.java
@@ -1,30 +1,52 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
     
     public static class Node{
-        int idx, count;
-        Node(int idx, int count){
+        int idx, time;
+        Node(int idx, int time){
             this.idx = idx;
-            this.count = count;
+            this.time = time;
         }
     }
     
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        List<Integer> answer = new ArrayList<>();
         Map<String, List<Node>> map = new HashMap<>();
+        Map<String, Integer> playtime = new HashMap<>();
         for(int i=0; i<genres.length; i++){
             String key = genres[i];
             List<Node> list;
             if(map.containsKey(key)){
-                list = map.get(key);                
+                list = map.get(key);                              
             }
             else {
-                list = new ArrayList<>();                
+                list = new ArrayList<>();
+                playtime.put(key,0);
             }
             list.add(new Node(i, plays[i]));
+            playtime.put(key, playtime.get(key) + plays[i]);
             map.put(key, list);
         }
+
+        List<String> keylist = new ArrayList<>(playtime.keySet());
+        Collections.sort(keylist, (o1, o2) -> {
+            return playtime.get(o2) - playtime.get(o1);
+        });
         
         
-        return answer;
+        for(String genre : keylist){
+            List<Node> list = map.get(genre);
+            Collections.sort(list, (o1, o2) -> {
+                if(o2.time == o1.time ) return o1.idx - o2.idx ;
+                return o2.time - o1.time ;
+            });
+            for(int i=0; i<2; i++){
+                answer.add(list.get(i).idx);                
+            }
+        }
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
