b933bccd8b07274fda0724d667b1ac7a8552daf7
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-08-20 23:09:27 +0900

    Java

--- LeetCode/Algorithms/Med/72.EditDistance[DP].java
+++ LeetCode/Algorithms/Med/72.EditDistance[DP].java
@@ -14,13 +14,19 @@ class Solution {
 
         for(int i=1; i<=N; i++){
             char a = word1.charAt(i-1);
-            for(int j=1; n<=M; j++){
+            for(int j=1; j<=M; j++){
                 char b = word2.charAt(j-1);
-
+                if(a==b) dp[i][j] = dp[i-1][j-1]; // 변화없음 
+                else {
+                    // dp[i-1][j] : 글자삭제
+                    // dp[i][j-1] : 글자추가 
+                    // dp[i-1][j-1] : 글자교체
+                    dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
+                }
             }
 
             
         }
-
+        return dp[N][M];
     }
 }
