38c45c7326353726f726fb46ed82fdd0a51f9ad0
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-09-08 22:17:15 +0900

    Java

--- LeetCode/Algorithms/Med/309.BestTimetoBuyandSellStockwithCooldown.java
+++ LeetCode/Algorithms/Med/309.BestTimetoBuyandSellStockwithCooldown.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        dp[0][0] = prices[0];
-        for(int i=0; i<n; i++){
+        int[][] dp = new int[n][3];
+        dp[0][0] = -prices[0];
+dp[0][1] = Integer.MIN_VALUE; 
+        for(int i=1; i<n; i++){
             int price = prices[i];
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][2])-price; 
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][2]-price); 
             dp[i][1] =dp[i-1][0] + price;
-            dp[i][2] = Math.max(dp[i-1][0], dp[i-1][1]);
+            dp[i][2] = Math.max(dp[i-1][2], dp[i-1][1]);
         }
 
-        return Math.max(dp[n-1][0], Math.max(dp[i-1][1], dp[i-1][2]));
+        return Math.max(dp[n-1][0], Math.max(dp[n-1][1], dp[n-1][2]));
     }
 }
