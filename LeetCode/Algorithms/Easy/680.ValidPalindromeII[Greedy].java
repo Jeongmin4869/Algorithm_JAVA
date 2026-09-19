b81b23ad1a07d803a91144ee2aa594949e01d6b0
Lee Jeongmin <45901815+Jeongmin4869@users.noreply.github.com>
2026-09-19 19:18:51 +0900

    Java

--- LeetCode/Algorithms/Easy/680.ValidPalindromeII[Greedy].java
+++ LeetCode/Algorithms/Easy/680.ValidPalindromeII[Greedy].java
@@ -1,5 +1,32 @@
 class Solution {
     public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length()-1;
+        while(l<=r){
+            if(s.charAt(l) != s.charAt(r)){                
+                return (check(s, l+1, r) || check(s, l, r-1));
+            }
+            l += 1;
+            r -= 1;
+        }
+
+        return true;
+    }
+
+    public static boolean check(String str, int left, int right){
+        while(left<right){
+            if(str.charAt(left) != str.charAt(right)) return false;
+            left += 1;
+            right -= 1;
+        }
+        return true;
+    }
+}
+
+/*
+
+class Solution {
+    public boolean validPalindrome(String s) {
 
         if(check(s)) return true;
 
@@ -25,3 +52,4 @@ class Solution {
         return true;
     }
 }
+*/
